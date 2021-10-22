c1bc39ad477e636acd49acd071bc6ba89075e333
maeshow <necobudou3011@gmail.com>
2021-10-22 16:45:02 +0900

    add look this way game

--- src/App.java
+++ src/App.java
@@ -5,32 +5,42 @@ public class App {
     private static final Scanner STDIN = new Scanner(System.in);
 
     private static final String[] HAND = { "グー", "チョキ", "パー" };
+    private static final String[] WAY = { "右", "左", "上", "下" };
     private static final int GUU_INDEX = 0;
     private static final int CHOKI_INDEX = 1;
     private static final int PAA_INDEX = 2;
-    private static final int FIRST_HAND_INDEX = 0;
-    private static final int LAST_HAND_INDEX = HAND.length - 1;
+    private static final int JANKEN_GAME_MODE = 0;
+    private static final int LOOK_THIS_WAY_GAME_MODE = 1;
+    private static final int TIMES_LIMIT = 3;
 
     private static boolean isDraw = false;
+    private static int playerWinCount = 0;
 
     public static void main(String[] args) {
         showWithNewLine(Messages.TITLE);
+        showFormattedMessage(Messages.GAME_TIMES_LIMIT, TIMES_LIMIT);
         showWithNewLine(Messages.JANKEN_START);
-        showInputDescription();
+        showInputDescription(JANKEN_GAME_MODE);
 
         startGame();
 
         STDIN.close();
     }
 
-    private static int getComputerHand() {
+    private static int getComputerchoice(int gameMode) {
         Random random = new Random();
-
-        return random.nextInt(HAND.length);
+        switch (gameMode) {
+        case JANKEN_GAME_MODE:
+            return random.nextInt(HAND.length);
+        case LOOK_THIS_WAY_GAME_MODE:
+            return random.nextInt(WAY.length);
+        default:
+            return 0;
+        }
     }
 
-    private static int getPlayerHandIndex() {
-        showWaitingInput();
+    private static int getPlayerChoiceIndex(int gameMode) {
+        showWaitingInput(gameMode);
         String input = STDIN.next();
         int inputNumber = 0;
         try {
@@ -38,43 +48,69 @@ public class App {
         } catch (NumberFormatException e) {
             showWithNewLine(Messages.ENTER_NUMBER_WARN);
             showNewLine();
-            return getPlayerHandIndex();
+            return getPlayerChoiceIndex(gameMode);
         }
 
-        if (isCorrectRange(inputNumber)) {
+        if (isCorrectRange(inputNumber, gameMode)) {
             return inputNumber;
         }
-        showFormattedMessage(Messages.NUMBER_MUST_BETWEEN_WARN, FIRST_HAND_INDEX, LAST_HAND_INDEX);
+        showWithNewLine(Messages.NUMBER_MUST_BETWEEN_WARN);
         showNewLine();
-        return getPlayerHandIndex();
+        return getPlayerChoiceIndex(gameMode);
     }
 
     private static void startGame() {
-        while (!isOverMaxChallengeTimes()) {
-            int playerHand = getPlayerHandIndex();
-            int computerHand = getComputerHand();
+        int count = 0;
+        while (!isOverMaxChallengeTimes(count)) {
+            startMatch();
+            count++;
+        }
+        showResultInfo(playerWinCount);
+    }
+
+    private static void startMatch() {
+        while (true) {
+            int playerHand = getPlayerChoiceIndex(JANKEN_GAME_MODE);
+            int computerHand = getComputerchoice(JANKEN_GAME_MODE);
             showFormattedMessage(Messages.RESULT, HAND[computerHand], HAND[playerHand]);
-            if (isDraw(playerHand, computerHand)) {
+            if (isDrawByJanken(playerHand, computerHand)) {
                 showWithNewLine(Messages.DRAW);
                 isDraw = true;
                 continue;
             }
-            if (isPlayerWin(playerHand, computerHand)) {
+            if (isPlayerWinByJanken(playerHand, computerHand)) {
                 showWithNewLine(Messages.WIN);
                 isDraw = false;
+                startLookThisWayMatch(true);
                 break;
             }
             showWithNewLine(Messages.LOSE);
             isDraw = false;
+            startLookThisWayMatch(false);
             break;
         }
     }
 
-    private static boolean isDraw(int player, int computer) {
+    private static void startLookThisWayMatch(boolean isPlayerWinByJanken) {
+        showWithNewLine(Messages.LOOK_THIS_WAY_START);
+        showInputDescription(LOOK_THIS_WAY_GAME_MODE);
+        int playerWay = getPlayerChoiceIndex(LOOK_THIS_WAY_GAME_MODE);
+        int computerWay = getComputerchoice(LOOK_THIS_WAY_GAME_MODE);
+        showWithNewLine(Messages.LOOK_THIS_WAY_END);
+        showFormattedMessage(Messages.RESULT, WAY[computerWay], WAY[playerWay]);
+        if (isPlayerWinByLookThisWay(playerWay, computerWay, isPlayerWinByJanken)) {
+            playerWinCount++;
+            showWithNewLine(Messages.WIN);
+        } else {
+            showWithNewLine(Messages.LOSE);
+        }
+    }
+
+    private static boolean isDrawByJanken(int player, int computer) {
         return player == computer;
     }
 
-    private static boolean isPlayerWin(int player, int computer) {
+    private static boolean isPlayerWinByJanken(int player, int computer) {
         switch (player) {
         case GUU_INDEX:
             return computer == CHOKI_INDEX;
@@ -87,28 +123,64 @@ public class App {
         }
     }
 
-    private static boolean isCorrectRange(int a) {
-        return 0 <= a && a < HAND.length;
+    private static boolean isPlayerWinByLookThisWay(int player, int computer, boolean isPlayerWinByJanken) {
+        if (isPlayerWinByJanken) {
+            return player == computer;
+        }
+        return player != computer;
     }
 
-    private static boolean isOverMaxChallengeTimes() {
-        return false;
+    private static boolean isCorrectRange(int a, int gameMode) {
+        switch (gameMode) {
+        case JANKEN_GAME_MODE:
+            return 0 <= a && a < HAND.length;
+        case LOOK_THIS_WAY_GAME_MODE:
+            return 0 <= a && a < WAY.length;
+        default:
+            return false;
+        }
     }
 
-    private static void showInputDescription() {
+    private static boolean isOverMaxChallengeTimes(int count) {
+        return TIMES_LIMIT <= count;
+    }
+
+    private static void showInputDescription(int gameMode) {
         int count = 0;
-        for (String str : HAND) {
+        String[] array = new String[0];
+        switch (gameMode) {
+        case JANKEN_GAME_MODE:
+            array = HAND;
+            break;
+        case LOOK_THIS_WAY_GAME_MODE:
+            array = WAY;
+            break;
+        }
+
+        for (String str : array) {
             showFormattedMessage(Messages.INPUT_DEFINE, count, str);
             count++;
         }
     }
 
-    private static void showWaitingInput() {
-        if (isDraw) {
-            showWithoutNewLine(Messages.WAITING_INPUT_FOR_DRAW);
-        } else {
-            showWithoutNewLine(Messages.WAITING_INPUT);
+    private static void showWaitingInput(int gameMode) {
+        switch (gameMode) {
+        case JANKEN_GAME_MODE:
+            if (isDraw) {
+                showWithoutNewLine(Messages.JANKEN_WAITING_INPUT_FOR_DRAW);
+            } else {
+                showWithoutNewLine(Messages.JANKEN_WAITING_INPUT);
+            }
+            break;
+        case LOOK_THIS_WAY_GAME_MODE:
+            showWithoutNewLine(Messages.LOOK_THIS_WAY_WAITING_INPUT);
+            break;
         }
+    }
+
+    private static void showResultInfo(int playerWinCount) {
+        showWithNewLine(Messages.RESULT_INFO);
+        showFormattedMessage(Messages.PLAYER_WIN_COUNT, playerWinCount);
     }
 
     private static void showWithNewLine(String message) {
@@ -123,11 +195,11 @@ public class App {
         System.out.println();
     }
 
-    private static void showFormattedMessage(String message, int a, String b) {
-        System.out.format(message, a, b);
+    private static void showFormattedMessage(String message, int a) {
+        System.out.format(message, a);
     }
 
-    private static void showFormattedMessage(String message, int a, int b) {
+    private static void showFormattedMessage(String message, int a, String b) {
         System.out.format(message, a, b);
     }
 
--- src/Messages.java
+++ src/Messages.java
@@ -1,14 +1,22 @@
 public class Messages {
-    public static final String TITLE = "じゃんけん勝負";
+    public static final String TITLE = "じゃんけん勝負&あっち向いてホイ！";
     public static final String JANKEN_START = "グーチョキパーを数字で入力してね";
+    public static final String GAME_TIMES_LIMIT = "%d回勝負だよ%n";
     public static final String INPUT_DEFINE = "%d:%s%n";
-    public static final String WAITING_INPUT = "最初はグー、じゃんけん： ";
-    public static final String WAITING_INPUT_FOR_DRAW = "あいこで： ";
+    public static final String JANKEN_WAITING_INPUT = "最初はグー、じゃんけん： ";
+    public static final String JANKEN_WAITING_INPUT_FOR_DRAW = "あいこで： ";
+
+    public static final String LOOK_THIS_WAY_START = "方向を数字で入力してね";
+    public static final String LOOK_THIS_WAY_WAITING_INPUT = "あっち向いて...： ";
+    public static final String LOOK_THIS_WAY_END = "ホイ！";
+
     public static final String RESULT = "%s(COM)と%s(Player)で...%n";
     public static final String WIN = "あなたの勝ち";
     public static final String LOSE = "あなたの負け";
     public static final String DRAW = "あいこだよ！";
+    public static final String RESULT_INFO = "-------結果-------";
+    public static final String PLAYER_WIN_COUNT = "あなたの勝利数： %d%n";
 
     public static final String ENTER_NUMBER_WARN = "数字を入力してください";
-    public static final String NUMBER_MUST_BETWEEN_WARN = "数字は%dから%dの範囲で指定してください%n";
+    public static final String NUMBER_MUST_BETWEEN_WARN = "正しい範囲の数字で指定してください";
 }
